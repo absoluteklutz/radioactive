f0f59dd7213c23cf36a2d38dc51e31fb72479853
klutz <kitsukashii@gmail.com>
2021-10-22 20:00:42 +0800

    fix package manager

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 package = input("package: ")
-getpackage = requests.get(f"https://radioactive.sudocode1.xyz/packages/{package}.radio").text
+getpackage = requests.get(f"https://radioactive.sudocode1.xyz/packages.php?{package}").text
 receivedpackage = open(package + '.radio', "w")
-receivedpackage.write(getpackage)
+receivedpackage.write(getpackage.replace('<br style="color: red;">', '\n'))
 print("done!")
